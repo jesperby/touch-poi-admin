f9a889bbae5aedede1adc51cb4132d2f365d5e76
Mattias Andersson <mattias.h.andersson@gmail.com>
2013-08-27 09:59:47 +0200

    Updating adress using openstreetmap when submittion position on map

--- sites/all/modules/custom/TPA_poi_content/js/map.js
+++ sites/all/modules/custom/TPA_poi_content/js/map.js
@@ -3,18 +3,63 @@
  */
 (function ($) {
   Drupal.behaviors.mapInit = {
+
+    /* 
+     * Function mapData()
+     * Function called from within iframed map, when user clicks submit
+     */
     mapData: function(data) {
-      $('#edit-field-latitude-und-0-value').val(data.north);
-      $('#edit-field-longitude-und-0-value').val(data.east);
+      // Set lat lon of chosen position
+      var lat = data.north;
+      var lon = data.east;
+
+      $('#edit-field-latitude-und-0-value').val(lat);
+      $('#edit-field-longitude-und-0-value').val(lon);
+
+      // Find nearest adress on openstreetmap
+      $.ajax({
+        type: 'GET',
+        url: 'http://nominatim.openstreetmap.org/reverse?format=json&lat='+lat+'&lon='+lon+'&zoom=18&email=kominteamet@malmo.se',
+        dataType: 'json',
+        success: function(json) {
+          // Update address field
+          var road = json['address']['road'];   // Ex: Drottninggatan
+          var house_number = json['address']['house_number'];  // Ex: 17 
+          var address = '';
+          
+          if( typeof road != 'undefined' ) {
+            address += road;
+            if( typeof house_number != 'undefined' ) {
+              address += ' ' + house_number;
+            }
+          }
+
+          $('#edit-field-adress-und-0-value').val(address);
+        },
+        error: function (request, status, error) {
+          console.log( 'request: ', request );
+          console.log( 'status: ', status );
+          console.log( 'error: ', error );
+        }
+      });
 
       // Reload map with marker on chosen position
       var map_coords = data.east + "," + data.north;
       $('#map_iframe').attr('src', 'http://localhost/msmap/index.html?config=eurov.js&&xy=' + map_coords);
     },
     
+
+    /* 
+     * Function map_loaded()
+     * Called after iframe containing map has been loaded
+     */
     map_loaded: function () {
     },
     
+
+    /* 
+     * Function attach()
+     */
     attach: function(context, settings) {
     }
   }
