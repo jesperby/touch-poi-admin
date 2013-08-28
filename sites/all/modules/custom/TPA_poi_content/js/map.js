71cfe5a07e8c86ec426bf632b744806c2e59a06d
Mattias Andersson <mattias.h.andersson@gmail.com>
2013-08-28 12:44:16 +0200

    Poi adress drop-down + updating map and lat/lon fields

--- sites/all/modules/custom/TPA_poi_content/js/map.js
+++ sites/all/modules/custom/TPA_poi_content/js/map.js
@@ -1,9 +1,9 @@
 /**
- * Drupal behavior for handling iframed Malmö Stad map
+ * Drupal behavior for handling Malmö Stad map data
  */
 (function ($) {
   Drupal.behaviors.mapHandler = {
-
+    
     /* 
      * Function mapData()
      * Function called from within iframed map, when user clicks submit
@@ -45,10 +45,94 @@
 
       // Reload map with marker on chosen position
       var map_coords = data.east + "," + data.north;
-      //console.log(this.getBaseUrl());
       $('#map_iframe').attr('src', 'http://' + this.getBaseUrl() + '/malmostad_map/index.html?config=eurov.js&xy=' + map_coords);
     },
+
+
+    /**
+     * Function to attach address drop down to add poi form
+     */
+     poi_address_dropdown: function() {
+      var that = this;
+      var $address;
+      $address = $("#edit-field-adress-und-0-value");
+      if ($address.length) {
+        $address.autocomplete({
+          source: function(request, response) {
+            return $.ajax({
+              url: "//xyz.malmo.se/rest/1.0/addresses/",
+              dataType: "jsonp",
+              data: {
+                q: request.term,
+                items: 25
+              },
+              success: function(data) {
+                return response($.map(data.addresses, function(item) {
+                  return {
+                    label: item.name + ", " + item.district,
+                    value: item.name,
+                    address: item.name,
+                    post_code: item.postcode,
+                    neighborhood: item.district,
+                    postal_town: item.postal_town,
+                    east: item.east,
+                    north: item.north
+                  };
+                }));
+              }
+            });
+          },
+          minLength: 2,
+          select: function(event, ui) {
+            // Set address field with chosen address
+            $('#user_address').val(ui.item.address);
+
+            // Convert address coordinates to google maps coordinates
+            var coords_espg_4326 = that.transform_epsg_3008_4326(ui.item.north, ui.item.east);
+            
+            // Reload map with marker on chosen address
+            var map_coords = coords_espg_4326.lon + "," + coords_espg_4326.lat;
+            $('#map_iframe').attr('src', 'http://' + that.getBaseUrl() + '/malmostad_map/index.html?config=eurov.js&xy=' + map_coords);
+            
+            // Set lat/lon fields of chosen address
+            $('#edit-field-latitude-und-0-value').val(coords_espg_4326.lat);
+            $('#edit-field-longitude-und-0-value').val(coords_espg_4326.lon);
+          },
+          open: function() {
+            //return $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+          },
+          close: function() {
+            //return $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+          }
+        });
+        return $('#user_address').keydown(function(event) {
+          if (event.which === 13) {
+            return event.preventDefault();
+          }
+        });
+      }
+    },
     
+
+    /**
+     * Function to transform coordinates from EPSG:3008 to EPSG:4326
+     */
+    transform_epsg_3008_4326: function(lat, lon) {
+      // Add espg definitions
+      Proj4js.defs["EPSG:3008"] = "+proj=tmerc +lat_0=0 +lon_0=13.5 +k=1 +x_0=150000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
+      Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+      
+      // creating source and destination Proj4js objects
+      var source = new Proj4js.Proj('EPSG:3008');    //source coordinates will be in Longitude/Latitude
+      var dest = new Proj4js.Proj('EPSG:4326');     
+
+      // transforming point coordinates
+      var p = new Proj4js.Point(lon, lat);   //any object will do as long as it has 'x' and 'y' properties
+      Proj4js.transform(source, dest, p);      //do the transformation.  x and y are modified in place
+
+      return {lon: p.x, lat: p.y};
+    },
+
 
     /* 
      * Function map_loaded()
@@ -62,6 +146,8 @@
      * Function attach()
      */
     attach: function(context, settings) {
+      // Attach autocomplete to add poi address field
+      this.poi_address_dropdown();
     },
 
     // Returns base + first directory, ex www.domain.com/directory
