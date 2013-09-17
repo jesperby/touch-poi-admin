1268359ec8686cd69238fbfbaae9a22353cd7fb5
Mattias Andersson <mattias.h.andersson@gmail.com>
2013-09-17 17:53:46 +0200

    Hiding/showing season fields in add poi form based on chosen poi category

--- sites/all/modules/custom/TPA_poi_content/js/disable_add_poi_fields.js
+++ sites/all/modules/custom/TPA_poi_content/js/disable_add_poi_fields.js
@@ -27,6 +27,7 @@
           var $pointType = $(this);
           var picturesEnabled = $pointType.attr('picturesEnabled');
           var closableEnabled = $pointType.attr('closableEnabled');
+          var seasonsEnabled = $pointType.attr('seasonsEnabled');
 
           // Enable/disable poi input fields
           if( picturesEnabled === 'false') {
@@ -39,6 +40,18 @@
             $('#edit-field-closed-und').attr('disabled','disabled');
           } else {
             $('#edit-field-closed-und').attr('disabled','');
+          }
+
+          if( seasonsEnabled === 'false') {
+            $('#edit-field-spring-dates').hide();
+            $('#edit-field-spring-times').hide();
+            $('#edit-field-autumn-dates').hide();
+            $('#edit-field-autumn-times').hide();
+          } else {
+            $('#edit-field-spring-dates').show();
+            $('#edit-field-spring-times').show();
+            $('#edit-field-autumn-dates').show();
+            $('#edit-field-autumn-times').show(); 
           }
        });
       }
