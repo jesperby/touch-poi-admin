54d1427bb9a71676c04199d50b84cf9a9b9d2699
Mattias Andersson <mattias.h.andersson@gmail.com>
2013-09-20 13:30:51 +0200

    Disabling fields when no poi category has been chosen

--- sites/all/modules/custom/TPA_poi_content/js/disable_add_poi_fields.js
+++ sites/all/modules/custom/TPA_poi_content/js/disable_add_poi_fields.js
@@ -4,23 +4,12 @@
 (function ($) {
   Drupal.behaviors.disableFields = {
     attach: function(context, settings) {
-      $( '#edit-field-subcategory-und' ).change( function() {
-        var selected_subcategory_id = $( '#edit-field-subcategory-und' ).val();
-        $.ajax({
-          type: 'GET',
-          url: Drupal.behaviors.TPA_poi_content_config.config['feed_url_poi_types'],
-          dataType: 'xml',
-          success: function(data) {
-            processData(data, selected_subcategory_id);
-          },
-          error: function (request, status, error) {
-            console.log( 'request: ', request );
-            console.log( 'status: ', status );
-            console.log( 'error: ', error );
-          }
-        });
-      });
-
+      
+      /**
+       * Function to process poi category data and to actually
+       * disable/enable fields based on the currently selected
+       * poi category 
+       */
       function processData(data, selected_subcategory_id) {
         var selector = "pointType[id=" + selected_subcategory_id + "]";
         $(data).find(selector).each(function () {
@@ -29,20 +18,22 @@
           var closableEnabled = $pointType.attr('closableEnabled');
           var seasonsEnabled = $pointType.attr('seasonsEnabled');
 
+          console.log( "sel in func", selected_subcategory_id );
+
           // Enable/disable poi input fields
-          if( picturesEnabled === 'false') {
+          if( picturesEnabled === 'false' ) {
             $('#edit-field-poi-image-und-0-upload').attr('disabled','disabled');
           } else {
             $('#edit-field-poi-image-und-0-upload').attr('disabled','');
           }
 
-          if( closableEnabled === 'false') {
+          if( closableEnabled === 'false' ) {
             $('#edit-field-closed-und').attr('disabled','disabled');
           } else {
             $('#edit-field-closed-und').attr('disabled','');
           }
 
-          if( seasonsEnabled === 'false') {
+          if( seasonsEnabled === 'false' ) {
             $('#node_poi_form_group_spring_dates').hide();
             $('#node_poi_form_group_autumn_dates').hide();
             $('#node_poi_form_group_spring_times').hide();
@@ -53,10 +44,46 @@
             $('#node_poi_form_group_spring_times').show();
             $('#node_poi_form_group_autumn_times').show(); 
           }
-       });
-      }
-    },
+        });
+      };
 
-    
+      /**
+       * Function to disable/enable fields based on currently
+       * selected poi category
+       */
+      function disable_enable_fields() {
+        var selected_subcategory_id = $( '#edit-field-subcategory-und' ).val();
+        
+        if( selected_subcategory_id === '_none' ) { // No poi category seleced, disable all
+          $('#edit-field-poi-image-und-0-upload').attr('disabled','disabled');
+          $('#edit-field-closed-und').attr('disabled','disabled');
+          $('#node_poi_form_group_spring_dates').hide();
+          $('#node_poi_form_group_autumn_dates').hide();
+          $('#node_poi_form_group_spring_times').hide();
+          $('#node_poi_form_group_autumn_times').hide();
+        } else {
+          $.ajax({
+            type: 'GET',
+            url: Drupal.behaviors.TPA_poi_content_config.config['feed_url_poi_types'],
+            dataType: 'xml',
+            success: function(data) {
+              processData(data, selected_subcategory_id);
+            },
+            error: function (request, status, error) {
+              console.log( 'request: ', request );
+              console.log( 'status: ', status );
+              console.log( 'error: ', error );
+            }
+          }); 
+        }
+      };
+
+      /**
+       * Init
+       */
+      disable_enable_fields(); 
+      $( '#edit-field-subcategory-und' ).change( disable_enable_fields );
+
+    }    
   }
 })(jQuery);
